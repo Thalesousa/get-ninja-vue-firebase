38fbb4132896fe961b4998d6dcc7998ce1ec8ea4
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-14 21:27:45 -0300

    77. Using Props in the Setup Function

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,45 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>Search Term</p>
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{name}}</p>
-    </div>
-
-    <button @click="handleClick">Click stop watch</button>
+    <PostList :posts="posts"/>
     
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from 'vue'
+import PostList from '@/components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  setup(){
-    const search = ref('')
-    const names = ref(['Mario', 'Luigi', 'Yoshi'])
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { id: 1, title: 'Post 1', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam aliquid laboriosam dignissimos blanditiis officiis ea? Asperiores illum accusamus eveniet quasi, consequatur laborum accusantium placeat error vero atque laudantium omnis harum illo magni, sit delectus iusto itaque expedita! Id labore voluptas, autem, pariatur adipisci non quia nulla temporibus, illo doloribus at repellat quod provident placeat tenetur minus quos corporis voluptatem? Consequuntur veniam est ab saepe aperiam excepturi odio quaerat voluptatem consectetur expedita debitis suscipit ex velit quos, molestiae maiores odit corrupti facilis tenetur alias voluptate hic? Vel nulla officia omnis quae quibusdam amet asperiores, nam nemo porro recusandae vitae autem unde molestias nisi consectetur, perspiciatis iusto. Quo dolorum pariatur hic ullam dolores quas quis modi, totam qui sint tenetur illum facere asperiores libero aliquam recusandae, nostrum provident nam? At laudantium molestiae eos architecto, ducimus iure sed odio, dicta officia reiciendis odit dolore placeat quasi cupiditate debitis ex ipsa et. Temporibus, veniam.'},
+      { id: 2, title: 'Post 2', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam aliquid laboriosam dignissimos blanditiis officiis ea? Asperiores illum accusamus eveniet quasi, consequatur laborum accusantium placeat error vero atque laudantium omnis harum illo magni, sit delectus iusto itaque expedita! Id labore voluptas, autem, pariatur adipisci non quia nulla temporibus, illo doloribus at repellat quod provident placeat tenetur minus quos corporis voluptatem? Consequuntur veniam est ab saepe aperiam excepturi odio quaerat voluptatem consectetur expedita debitis suscipit ex velit quos, molestiae maiores odit corrupti facilis tenetur alias voluptate hic? Vel nulla officia omnis quae quibusdam amet asperiores, nam nemo porro recusandae vitae autem unde molestias nisi consectetur, perspiciatis iusto. Quo dolorum pariatur hic ullam dolores quas quis modi, totam qui sint tenetur illum facere asperiores libero aliquam recusandae, nostrum provident nam? At laudantium molestiae eos architecto, ducimus iure sed odio, dicta officia reiciendis odit dolore placeat quasi cupiditate debitis ex ipsa et. Temporibus, veniam.'},
+      { id: 3, title: 'Post 3', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam aliquid laboriosam dignissimos blanditiis officiis ea? Asperiores illum accusamus eveniet quasi, consequatur laborum accusantium placeat error vero atque laudantium omnis harum illo magni, sit delectus iusto itaque expedita! Id labore voluptas, autem, pariatur adipisci non quia nulla temporibus, illo doloribus at repellat quod provident placeat tenetur minus quos corporis voluptatem? Consequuntur veniam est ab saepe aperiam excepturi odio quaerat voluptatem consectetur expedita debitis suscipit ex velit quos, molestiae maiores odit corrupti facilis tenetur alias voluptate hic? Vel nulla officia omnis quae quibusdam amet asperiores, nam nemo porro recusandae vitae autem unde molestias nisi consectetur, perspiciatis iusto. Quo dolorum pariatur hic ullam dolores quas quis modi, totam qui sint tenetur illum facere asperiores libero aliquam recusandae, nostrum provident nam? At laudantium molestiae eos architecto, ducimus iure sed odio, dicta officia reiciendis odit dolore placeat quasi cupiditate debitis ex ipsa et. Temporibus, veniam.'}
+    ])
 
-    const stopWatch = watch(search, () => {
-      console.log('Watch')
-    })
-
-    const stopWatchEffect = watchEffect(() => {
-      console.log('Watch Effect', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter(name => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopWatchEffect()
-    }
-    
-
-    return {search, matchingNames, handleClick}
-  },
+    return { posts }
+  }
 }
 </script>
