76244c9270d0d2a17138dedf4dacf68c1c34d778
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-19 00:08:36 -0300

    100. RealTime listeners

--- dojo-blog/src/views/RealTimeView.vue
+++ dojo-blog/src/views/RealTimeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <div v-if="error">{{error}}</div>
+    <h1>RealTime</h1>
     <div v-if="posts.length" class="layout">
       <PostList :posts="posts"/>
       <TagCloud :posts="posts"/>
@@ -13,32 +12,32 @@
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import { projectFirestore } from '@/firebase/config'
+
 import PostList from '@/components/PostList.vue'
 import TagCloud from '@/components/TagCloud.vue'
 import Spinner from '@/components/Spinner.vue'
-import { getPosts } from '@/composables/getPosts'
 
 export default {
-  name: 'HomeView',
   components: { PostList, TagCloud, Spinner },
   setup() {
-    const { posts, error, load } = getPosts()
-    load()
-    return { posts, error }
+    const posts = ref([])
+    projectFirestore.collection('posts')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(snap => {
+        let docs = snap.docs.map(doc => {
+          return { ...doc.data(), id: doc.id }
+        })
+
+        posts.value = docs
+      })
+
+    return { posts }
   }
 }
 </script>
 
 <style>
-.home .realtime {
-  margin: 0 auto;
-  max-width: 1200px;
-  padding: 10px;
-}
 
-.layout {
-  display: grid;
-  grid-template-columns: 3fr 1fr;
-  gap: 20px;
-}
 </style>
