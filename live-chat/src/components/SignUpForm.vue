ca91a303382009f3eed88ca0e22db9f6b1df1326
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-19 13:57:27 -0300

    Page Login and SingUp

---
+++ live-chat/src/components/SignUpForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="display name" v-model="displayName">
+    <input type="email" placeholder="email" v-model="email">
+    <input type="password" placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const displayName = ref('')
+    const email = ref('')
+    const password = ref('')
+
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value)
+    }
+
+    return { displayName, email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
