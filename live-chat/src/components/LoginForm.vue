1f488625da682c9d86c8a5cbaa1a4aeca4cb24ad
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-19 20:08:36 -0300

    Creating a Login Composable

--- live-chat/src/components/LoginForm.vue
+++ live-chat/src/components/LoginForm.vue
@@ -3,21 +3,28 @@
     <input type="email" placeholder="email" v-model="email">
     <input type="password" placeholder="password" v-model="password">
     <button>Log in</button>
+    <div class="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { useLogin } from '@/composables/useLogin'
 export default {
   setup() {
     const email = ref('')
     const password = ref('')
 
-    const handleSubmit = () => {
-      console.log(email.value, password.value)
+    const { error, login } = useLogin()
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value)
+      if(!error.value) {
+        console.log('user logged in')
+      }
     }
 
-    return { email, password, handleSubmit }
+    return { email, password, error, handleSubmit }
   }
 }
 </script>
