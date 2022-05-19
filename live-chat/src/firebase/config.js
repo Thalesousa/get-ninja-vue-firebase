ef5c340728b0c5e4b83a57000701aa7e26b1ebe9
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-19 13:07:22 -0300

    Firebase setup

---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAo0ELXqMBtW8YxwsQKEKH4-S7LVebO-8Q",
+  authDomain: "tjsb-vue-chat.firebaseapp.com",
+  projectId: "tjsb-vue-chat",
+  storageBucket: "tjsb-vue-chat.appspot.com",
+  messagingSenderId: "272726093699",
+  appId: "1:272726093699:web:43c01edda5af14c57e10b6"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
