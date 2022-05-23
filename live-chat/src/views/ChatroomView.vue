f6349518d21f05a130207f095fec1944434e55a0
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-23 20:12:28 -0300

    Watching the Current User

--- live-chat/src/views/ChatroomView.vue
+++ live-chat/src/views/ChatroomView.vue
@@ -6,8 +6,21 @@
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import { getUser } from '@/composables/getUser'
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
 export default {
-  components: { NavBar }
+  components: { NavBar },
+  setup() {
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch(user, () => {
+      if(!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 
