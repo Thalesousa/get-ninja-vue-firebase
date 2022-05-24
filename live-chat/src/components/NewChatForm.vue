b0f79319518b7022b2f4014e7bb618d10a6edb4a
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-23 21:29:17 -0300

    117. Adding New Messages to Firestore

--- live-chat/src/components/NewChatForm.vue
+++ live-chat/src/components/NewChatForm.vue
@@ -1,45 +1,45 @@
 <template>
   <form>
     <textarea
-      placeholder="Type a message and hit enter to send..."
+      placeholder="Type a message and hit enter to send..." 
       v-model="message"
       @keypress.enter.prevent="handleSubmit"
     ></textarea>
+    <div v-if="error" class="error">{{ error }} blah</div>
   </form>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { getUser } from '@/composables/getUser'
-import { timestamp } from '@/firebase/config'
+import {useCollection} from '../composables/useCollection'
+import {getUser} from '../composables/getUser'
+import { timestamp } from '../firebase/config'
+import { ref } from 'vue'
 export default {
   setup() {
     const { user } = getUser()
-
+    const { addDoc, error } = useCollection('messages')
+    // refs
     const message = ref('')
-
     const handleSubmit = async () => {
       const chat = {
         name: user.value.displayName,
         message: message.value,
         createdAt: timestamp()
       }
-
-      console.log(chat)
-      message.value = ''
+      await addDoc(chat)
+      if (!error.value) {
+        message.value = ''
+      }
     }
-
-    return { message, handleSubmit }
-  },
+    return { message, handleSubmit, error }
+  }
 }
 </script>
-
 
 <style scoped>
   form {
     margin: 10px;
   }
-
   textarea {
     width: 100%;
     max-width: 100%;
@@ -50,5 +50,11 @@ export default {
     border-radius: 20px;
     font-family: inherit;
     outline: none;
+  }
+  .error {
+    text-align: center;
+    color: #ff2a58;
+    font-size: 12px;
+    padding: 10px 0;
   }
 </style>
