6b9ea54e2c5c8a1e052f2d0d829aab23c09b8ba4
Thales Sousa <thales.sousa@edupulses.com.br>
2022-05-24 22:48:08 -0300

    Create chat window

---
+++ live-chat/src/composables/getCollection.js
@@ -0,0 +1,24 @@
+import { projectFirestore } from "@/firebase/config"
+import { ref } from "vue"
+
+export const getCollection = (collection) => {
+  const documents = ref(null)
+  const error = ref(null)
+
+  let collectionRef = projectFirestore.collection(collection).orderBy('createdAt')
+  
+  collectionRef.onSnapshot(snap => {
+    let results = []
+    snap.docs.forEach(doc => {
+      doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+    })
+    documents.value = results
+    error.value = null
+  }, (err) => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'could not fetch data'
+  })
+
+  return { error, documents }
+}
